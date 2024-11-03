ed1293886e1db93f3010c4bd7c56b4bd583652ab
karnhao <srv33605@gmail.com>
2024-11-03 15:02:03 +0700

    enable login feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 // src/App.tsx
-import React from "react";
+import React, { useState } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Section } from "./components/section/Section";
 import Sidebar from "./components/Sidebar";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <div style={{ display: 'flex' }}>
-        <Sidebar />
+        {isAuthenticated && <Sidebar />}
         <main style={{ flex: 1, padding: 20 }}>
           <Routes>
-            <Route path="/" element={<Section />} />
+            <Route path="/" element={isAuthenticated ? <Section /> : <LoginPage setIsAuthenticated={setIsAuthenticated} />} />
             <Route path="/news-feed" element={<div>News Feed Page</div>} />
             <Route path="/kanbanboard" element={<Section />} />
             <Route path="/project-1" element={<div>Project 1</div>} />
             <Route path="/project-2" element={<div>Project 2</div>} />
             <Route path="/project-3" element={<div>Project 3</div>} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </main>
       </div>
